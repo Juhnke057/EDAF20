09ae22866577723eaf511214e68c7e31205c9615
Viktor Juhnke <57365322+Juhnke057@users.noreply.github.com>
2022-02-02 21:41:11 +0100

    Update Lab2.sql

--- Lab2/Lab2.sql
+++ Lab2/Lab2.sql
@@ -1,5 +1,5 @@
-use sys;
--- Enable the use of foreign_keys in SQLite, by default OFF!
+use new_schema;
+
 set foreign_key_checks = 0;
 drop table if exists usershow;
 drop table if exists theater;
@@ -7,6 +7,7 @@ drop table if exists movie;
 drop table if exists movieShow;
 drop table if exists ticket;
 set foreign_key_checks = 1;
+
 create table usershow(
 userName varchar(50) not NULL,
 realName varchar(50) not NULL,
@@ -16,7 +17,7 @@ primary key(userName)
 
 create table theater(
 theaterName varchar(50) not null,
-nbrSeats Integer not null,
+nbrSeats Integer not null check (nbrSeats > 0),
 primary key(theaterName)
 );
 
@@ -62,9 +63,9 @@ insert into movieShow(movieShowID, theaterName, MovieID, showDate) values
 ('1112','Filmstaden Malmö','spoder123','2022-03-21'),
 ('1234','Filmstaden Lund','scary5','2023-12-24');
 insert into ticket(resNbr, userName, movieShowID) values
-('111','Viktor','1111'),
-('112','ReglerKungen123','1112'),
-('123','Stockholm_Suger','1234');
+('0','Viktor','1111'),
+('0','ReglerKungen123','1112'),
+('0','Stockholm_Suger','1234');
 -- b
 select movieName from movieShow
 join movie on movieShow.movieID = movie.movieID;
@@ -74,7 +75,22 @@ from movieShow;
 
 select * 
 from MovieShow;
+select *
+from ticket;
 -- c 
--- ??????
+-- kolla ticket
 
 -- D8
+-- insert into theater(theaterName, nbrSeats) value
+-- ('Filmstaden Helsingborg', 700);
+-- "Error Code: 1062. Duplicate entry 'Filmstaden Helsingborg' for key theater.Primary"
+
+-- insert into movieShow(movieShowID, theaterName, MovieID, showDate) values
+-- ('1224','Totaly Not Filmstaden','scary5','2023-12-24');
+-- "Error Code: 1452. Cannot add or update a child row"
+
+-- D9
+-- If there are 1 seat avaliable for a show and two people make buy a ticket at the same time there will be one to many bookings
+
+-- D10
+-- Solution: Make it so that only one order can be made at a time
